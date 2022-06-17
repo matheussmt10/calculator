a024245f64c0650c04488f3e7b34ab157d32526b
Matheus Matos <matheusmatosmatheus@gmail.com>
2022-06-17 10:47:09 -0300

    adding calculate files to the repository

---
+++ script.js
@@ -0,0 +1,31 @@
+// funcionalidade dos botões
+
+
+const button = document.querySelectorAll('#button')
+const numbers = document.querySelector('#numbers')
+const screen = document.querySelector('#screen')
+
+/*button.forEach((number, index) => {
+    number.addEventListener('click', () => {
+        const screen = document.getElementById('screen');
+        nt = number.textContent;
+        n = Number(nt);
+        
+        screen.innerHTML += nt;
+        console.log(typeof(nt))
+    })
+}); */
+
+function insert(value) {
+    screen.innerHTML += value;
+}
+
+function clean() {
+    screen.innerHTML = '';
+}
+
+function confirmCal() {
+    if (screen.innerHTML != 'Erro') {
+        screen.innerHTML = eval(screen.innerHTML)
+    }
+}
